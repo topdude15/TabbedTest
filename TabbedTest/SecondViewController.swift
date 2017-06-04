319e96aeaebe56767984ea0e88137e6ea8c05bf4
topdude15 <topdude15@gmail.com>
2017-06-04 17:41:27 -0400

    Initial Commit

---
+++ TabbedTest/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  TabbedTest
+//
+//  Created by Trevor Rose on 6/4/17.
+//  Copyright © 2017 Trevor Rose. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
